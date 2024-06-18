7d6ca2f4c89be8b81754536e645ba4f12e7cdcf0
IvanMarte <ivanbiasfx@gmail.com>
2024-06-18 12:20:55 -0600

    Couse Copilot v1.0

---
+++ prompts.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+course_name = "Sistemas de recomendación con Machine Learning"
+
+target_audience = "Personas con conocimiento en programación con Python que buscan profundizar su conocimiento en machine learning y crear un sistema de recomendación para su empresa, tienen un trabajo en una empresa top de México, cuentan con 4 horas a la semana para estudiar de lunes a viernes"
+
+
+specific_topics = "Ejemplos y casos de uso de sistemas de recomendación"
+course_level = "avanzado"
+
+course_focus = "técnico"
+
+next_learning_unit = "IA Generativa"
