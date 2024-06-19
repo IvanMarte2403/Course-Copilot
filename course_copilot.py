82efbefc0a530b955ee9d4b2255b741afdf1b42b
IvanMarte <ivanbiasfx@gmail.com>
2024-06-19 15:59:46 -0600

    fix: credentials

--- course_copilot.py
+++ course_copilot.py
@@ -17,8 +17,8 @@ scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/au
 
 openai.api_key = st.secrets["gpt_key"]['api_key']
 
-json_creds = st.secrets["google_creds"]["json"]
-json_creds_str = json.dumps(json_creds)
+json_creds_str = st.secrets["google_creds"]["json"]
+json_creds = json.loads(json_creds_str)
 creds = ServiceAccountCredentials.from_json_keyfile_dict(json_creds, scope)
 
 client = gspread.authorize(creds)
